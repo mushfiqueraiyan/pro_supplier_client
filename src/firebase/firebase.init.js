e4cb331397cd2ce8cd2a4b3347aae4a642b74984
mushfiqueraiyan <mushfiqueraiyan1@gmail.com>
2025-06-22 15:23:39 +0600

    added environment variable on firebase

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBIFYMQi8YiKtnNml9K6BZkuRM5wQZ14gw",
-  authDomain: "pro-supplier.firebaseapp.com",
-  projectId: "pro-supplier",
-  storageBucket: "pro-supplier.firebasestorage.app",
-  messagingSenderId: "905651705886",
-  appId: "1:905651705886:web:80bc601acde31d0834360f",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 const app = initializeApp(firebaseConfig);
